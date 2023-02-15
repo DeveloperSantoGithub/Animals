1207bf50786589e40d7e64f33ecaaa54810f9f4a
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-02-16 01:00:08 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import ShowAnimals from './components/ShowAnimals';
 
 function App() {
+	const handleClick = () => {
+		console.log('New animals added');
+	};
+
 	return (
 		<>
 			<h1>Animals Show</h1>
 			<ShowAnimals />
+
+			<button onClick={handleClick}>Add Animals</button>
 		</>
 	);
 }
